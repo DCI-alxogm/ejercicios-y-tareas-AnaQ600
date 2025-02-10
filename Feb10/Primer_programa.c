5d37f97a56b12cb1950b213c11ad1388be28ef13
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-02-10 11:25:54 -0600

    Primer programa

---
+++ Feb10/Primer_programa.c
@@ -0,0 +1,8 @@
+/*Creado el 10 de Febrero de 2025 por Ana Quiroz*/
+#include<stdio.h>
+int main ()
+{
+	printf ("Hola Ana \n");
+	printf ("Este es el primer programa del curso \n");
+}
+/*Aqui termina el programa*/
