a800b902442c68fdaf840a7d55203c90c037ad6a
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-02-10 11:51:59 -0600

    Segundo Programa

---
+++ Feb10/Segundo_programa.c
@@ -0,0 +1,9 @@
+/*Este es el segundo programa del 10 de febrero del 2025*/
+#include<stdio.h>
+int main ()
+{
+	printf ("Hola Ana \n");
+	printf ("Este es el segundo programa ejercicio \n");
+}
+
+/*Aqui termina el programa*/
