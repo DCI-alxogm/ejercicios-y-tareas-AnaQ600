b8c76372aecf73cf8088b84124d153c083bfd170
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-02-19 10:47:32 -0600

    Corregir tarea

--- Tareas/Tarea_operaciones_int_float.c
+++ Tareas/Tarea_operaciones_int_float.c
@@ -4,21 +4,20 @@
 
 int main ()
 {
-int a,b,c,d;
-int e1, e2, e3, e4;
 
-a =0;
-b=0;
-c=0;
-d=0;
-e1=(a+b) * c/d;
-e2=( (a+b)*c)/d;
-e3=(a+b) * (c/d);
-e4=a+b*c/d;
+	float a,b,c,d;
+	float e1, e2, e3, e4;
+
 	printf("Ingresa 4 valores para poder realizar las operaciones y mostrarte los resultados");
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	printf("e=(a+b) * c/d=%d \n", e1);
-	printf("e=( (a+b)*c)/d=%d \n", e2);
-	printf("e=(a+b) * (c/d)=%d \n", e3);
-	printf("e=a+b*c/d=%d \n", e4);
+	scanf("%f %f %f %f",&a,&b,&c,&d);
+	e1=(a+b) * c/d;
+	e2=( (a+b)*c)/d;
+	e3=(a+b) * (c/d);
+	e4=a+b*c/d;
+
+
+	printf("e=(a+b) * c/d=%f \n", e1);
+	printf("e=( (a+b)*c)/d=%f \n", e2);
+	printf("e=(a+b) * (c/d)=%f \n", e3);
+	printf("e=a+b*c/d=%f \n", e4);
 }
