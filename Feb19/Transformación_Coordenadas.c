bec3c08f01c2896d121257e2a474d79bf7ff70e0
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-02-20 14:31:32 -0600

    Transformacion de coordenadas

--- Feb19/Transformación_Coordenadas.c
+++ Feb19/Transformación_Coordenadas.c
@@ -8,14 +8,22 @@ int main()
 {
 	float x,y,z;
 	float r,teta,fi;
+	float rc,tetaC,zeta;
 
 	printf("Introduce las coordenadas cartesianas: \n");
-	scanf ("%f %f %f", &x, &y, &z);
+	scanf("%f %f %f", &x, &y, &z);
 	
-	r= sqrt ((x*x) + (y*y) + (z*z));
-	teta= atan (y/x);
-	fi= acos (z / sqrt ((x*x) + (y*y) + (z*z)));
+	r=sqrt((x*x) + (y*y) + (z*z));
+	teta=atan(y/x);
+	fi=acos(z / sqrt ((x*x) + (y*y) + (z*z)));
 
-	printf("Las coordenadas esféricas son: r=%f, teta=%f y fi=%f\n", r,teta,fi);
+	rc=sqrt((x*x)+(y*y));
+	tetaC=atan(x/y);
+	zeta=z;
+
+	printf("Las coordenadas esfericas son: r=%f, teta=%f y fi=%f\n", r,teta,fi);
+	printf("Las coordenadas cilindricas son: rc=%f,tetaC=%f y zeta=%f", rc,tetaC,zeta);
+
+	
 
 }
