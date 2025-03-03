6c674ae63914f8968f2609d86b64216d65a0c47b
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-03-03 11:31:09 -0600

    Saber si numeros son par o impar

---
+++ Mar3/Par_impar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int numero, residuo;
+    char opc[3];  // Increased the size to 3 to handle the "si" or "no" input
+
+leernumero:
+    printf("Introduce un numero para saber si es par o impar\n");
+    scanf("%i", &numero);
+    residuo = numero % 2;
+
+    if (residuo == 0)
+    {
+        printf("El numero %i es par\n", numero);
+    }
+    else
+    {
+        printf("El numero %i es impar\n", numero);
+    }
+
+    printf("¿Quieres revisar otro numero? (si/no)\n");
+    scanf("%s", opc);  // Don't need '&' for the array
+    
+    if (strcmp(opc, "si") == 0)
+    {
+        goto leernumero;
+    }
+    else if (strcmp(opc, "no") == 0)
+    {
+        printf("Hasta la proxima\n");
+    }
+    else
+    {
+        printf("Solo acepto si o no\n");
+    }
+
+    return 0;
+}
