d6b367d09a2e48fd8b7eb55eb240c2833f7c006e
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-03-03 11:52:52 -0600

    Calculadora

---
+++ Mar3/Calculadora.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int temp, coor;
+	printf("Ingrese un 1 si desea la opcion de Conversion de temperaturas o un 2 si desea el cambio de coordenadas a esfericas o
+}
