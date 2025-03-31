e8de9b44bdb825f5eef6fa2ec11c5da21883fbca
AnaQ600 <ak.quirozrojas@ugto.mx>
2025-03-31 09:52:50 -0600

    Ejercicio 1 For

--- Mar26/Ejemplo_1_For.c
+++ Mar26/Ejemplo_1_For.c
@@ -1,18 +1,35 @@
-#include<stdio.h>
-
-int main()
-{
-	float temp_C, temp_K;
-	float Temp_C=100, final=200,delta;
-	int n=10, i; 
-
-	delta=(final-Temp_C)/n;
-	for(i=0;i<n;i++)
-	{
-		temp_K=temp_C+273.15;
-		printf("%f %f\n", temp_C, temp_K);
-		temp_C=temp_C+delta;		//temp_C+=delta;
-
-	}
-return(0);
+#include <stdio.h>
+ 
+int main() {
+    float temp_C, temp_K;
+    float final, delta;
+    int n, i;
+    char repetir;
+ 
+    do {
+        
+        printf("Ingrese la temperatura inicial en Celsius: ");
+        scanf("%f", &temp_C);
+        printf("Ingrese la temperatura final en Celsius: ");
+        scanf("%f", &final);
+        printf("Ingrese el número de pasos: ");
+        scanf("%d", &n);
+ 
+        
+        delta = (final - temp_C) / n;
+ 
+        
+        for (i = 0; i <= n; i++) {
+            temp_K = temp_C + 273.15;
+            printf("%f °C  %f K\n", temp_C, temp_K);
+            temp_C += delta;
+        }
+ 
+        
+        printf("¿Desea ejecutar nuevamente? (s/n): ");
+        scanf(" %c", &repetir);
+ 
+    } while (repetir == 's' || repetir == 'S');
+ 
+    return 0;
 }
